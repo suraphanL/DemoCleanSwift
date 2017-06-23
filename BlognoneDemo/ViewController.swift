601c0227e6a6df3a28d885e7474a0ae4bdb05a7b
Suraphan 'Rawd' Laokondee <Suraphan.L@kaidee.com>
2017-06-23 20:11:32 +0700

    Initial Commit

---
+++ BlognoneDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlognoneDemo
+//
+//  Created by Suraphan 'Rawd' Laokondee on 6/23/2560 BE.
+//  Copyright © 2560 Suraphan 'Rawd' Laokondee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
